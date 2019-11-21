353c7358b84e777c3d96d4563b0c3322392c836e
Changbeom Choi <cbchoi@handong.edu>
2019-11-21 08:45:17 +0000

    inital

--- cal.py
+++ cal.py
@@ -1,1 +1,10 @@
-print('web_cal')
+import pymongo
+conn = pymongo.MongoClient('mongodb://db:27017')
+db = conn.get_database('web_cal')
+
+col_user = db.get_collection('user')
+
+col_user.delete_many({})
+
+user = [{'user_id':0, 'email': 'cbchoi@handong.edu', 'rank':0}]
+col_user.insert_many(user)
