5765d63289808241f8df692060769425ba185bf9
root <21400229@handong.edu>
2019-11-25 19:07:26 +0000

    event

--- sitapp/app/event/views.py
+++ sitapp/app/event/views.py
@@ -7,6 +7,7 @@ from wtforms.validators import Required, length
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 
+'''
 conn = pymongo.MongoClient('mongodb://db:27017')
 db = conn.get_database("events")
 collection = db.get_collection('time')
@@ -15,7 +16,7 @@ conn2 = pymongo.MongoClient('mongodb://db:27017')
 db1 = conn2.get_database("events")
 collection2 = db.get_collection("location")
 
-'''
+
 results = collection.find()
 [print(result) for result in results]
 print(results)
@@ -42,52 +43,91 @@ day = []
 for i in range(1,32):
     day.append((str(i),str(i)))
 
+
+def set_Mongo():
+	conn = pymongo.MongoClient('mongodb://db:27017')
+	db = conn.get_database('web_cal')
+	col_event = db.get_collection('event')
+	#col_event.delete_many({})
+	print(col_event.find())
+	return col_event
+
+
+
+
+
 class NameForm(FlaskForm):
-    #name = StringField('What is your schedules', validators=[Required()])
-        #validators=[Required()]
-    year = SelectField("년",choices = year,validators=[Required()],default=date.today().year)
-    month = SelectField("월", choices=month,validators=[Required()],default=date.today().month)
-    day = SelectField("일",choices = day,validators=[Required()],default=date.today().day)
-    schedules = StringField('이벤트를 입력하세요', validators=[Required()])
-    location = StringField('장소를 입력하세요', validators=[Required()])
-    submit = SubmitField('추가')
+	#name = StringField('What is your schedules', validators=[Required()])
+	    #validators=[Required()]
+	year = SelectField("년",choices = year,validators=[Required()],default=date.today().year)
+	month = SelectField("월", choices=month,validators=[Required()],default=date.today().month)
+	day = SelectField("일",choices = day,validators=[Required()],default=date.today().day)
+	schedules = StringField('이벤트를 입력하세요', validators=[Required()])
+	location = StringField('장소를 입력하세요', validators=[Required()])
+	submit = SubmitField('추가')
 ######
+
 
 class DeleteButton(FlaskForm):
     submit = SubmitField("일정삭제")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    d_button = DeleteButton()
-    events = []
-    if form.validate_on_submit():
-        flash('added a new schedules')
-        session['year'] = form.year.data
-        session['month'] = form.month.data
-        session['day'] = form.day.data
-        year = session.get('year')
-        month = session.get('month')
-        day = session.get('day')
-        date = str(year) + "-" + str(month) + "-" + str(day)
-        session['schedules'] = form.schedules.data
-        event = session.get('schedules')
-        session['location'] = form.location.data
-        location = session.get('location')
-        form.schedules.data = ''
-        collection.insert_one({"date":date, "event": event})
-        collection2.insert_one({"date":date, "location": location})
-        return redirect(url_for('index'))
-    return render_template('index.html',  form = form,
-                           year = session.get('year'), month = session.get('month'),
-                           day = session.get('day'), location = session.get('location'))
+	form = DeleteButton()
+	col_event = set_Mongo()
+	cursor = col_event.find()
+	reults = []
+	for event in cursor:
+		results.append(event)
+		
+	if form.validate_on_submit():
+		return redirect(url_for('create'))
+	return render_template('main.html', rseults = results, form = form)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+	form = NameForm()
+	#d_button = DeleteButton()
+	#events = []
+	if form.validate_on_submit():
+		col_event = set_Mongo()
+		flash('added a new schedules')
+		#session['year'] = form.year.data
+		#session['month'] = form.month.data
+		#session['day'] = form.day.data
+		#year = session.get('year')
+		#month = session.get('month')
+		#day = session.get('day')
+		year = form.year.data
+		month = form.month.data
+		day = form.day.data
+		date = str(year) + "-" + str(month) + "-" + str(day)
+		
+		#session['schedules'] = form.schedules.data
+		#event = session.get('schedules')
+		#session['location'] = form.location.data
+		#location = session.get('location')
+
+		schedules = form.schedules.data
+		location = form.location.data
+		name = date+","+schedules
+
+		#form.schedules.data = ''
+		col_event.insert_one({"name": name, "date":date, "location": location, "schedules": schedules})
+		results = col_event.find()
+		[print(result) for result in results] 
+		return redirect(url_for('index'))
+	return render_template('create.html',  form = form,
+		year = session.get('year'), month = session.get('month'),
+		day = session.get('day'), location = session.get('location'), schedules = session.get('schedules'))
+
+
+
+
+#@app.route('/user/<name>')
+#def user(name):
+#	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-
-collection.find()
+	app.run(debug=True, host='0.0.0.0')
