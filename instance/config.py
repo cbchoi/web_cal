0c05feae9291e424052d4f9ea9aa19fd7b276dd4
yoonyire <48197273+yoonyire@users.noreply.github.com>
2019-12-02 11:12:06 +0900

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -2,8 +2,8 @@ MAIL_SERVER = 'smtp.googlemail.com' # Hostname or IP address of the email server
 MAIL_PORT = 587 # Port of the email server
 MAIL_USE_TLS = True # Enable Transport Layer Security (TLS) security
 #MAIL_USE_SSL = False # Enable Secure Sockets Layer (SSL) security
-MAIL_USERNAME = '21400488@handong.edu' #Mail account username
-MAIL_PASSWORD = 'cgifuyedoxfmrgmu' # Mail account password
+MAIL_USERNAME = '' #Mail account username
+MAIL_PASSWORD = '' # Mail account password
 
 MAIL_SUBJECT_PREFIX = 'SIT32006'
 MAIL_SENDER = 'try@web.com'
