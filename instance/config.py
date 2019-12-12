c6f71f2a8e90adc6080e8937702a26659fa32b3b
yoonyire <21400488@handong.edu>
2019-12-12 08:34:12 +0000

    password removed

--- instance/config.py
+++ instance/config.py
@@ -2,10 +2,10 @@ MAIL_SERVER = 'smtp.googlemail.com' # Hostname or IP address of the email server
 MAIL_PORT = 587 # Port of the email server
 MAIL_USE_TLS = True # Enable Transport Layer Security (TLS) security
 #MAIL_USE_SSL = False # Enable Secure Sockets Layer (SSL) security
-MAIL_USERNAME = '21400488@handong.edu' #Mail account username
-MAIL_PASSWORD = 'wmcrpktyxmdcwubr' # Mail account password
+MAIL_USERNAME = '' #Mail account username
+MAIL_PASSWORD = '' # Mail account password
 
-MAIL_SUBJECT_PREFIX = 'SIT32006'
+MAIL_SUBJECT_PREFIX = 'Web_cal'
 MAIL_SENDER = 'try@web.com'
 
-ADMIN = "21400488@handong.edu"
+ADMIN = ""
